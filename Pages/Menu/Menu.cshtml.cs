59feb2b72003e49872f60ee5be3e205f3c5732a6
Tony Creeney <tonycreeney@gmail.com>
2023-11-16 04:29:04 +0000

    Add project files.

---
+++ Pages/Menu/Menu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GrillionaireWepApp.Pages.Menu
+{
+    public class MenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
